c04cba27796a914a04242179d7722ab77a8ab3d4
xx8086 <liuan333@126.com>
2018-01-28 18:24:06 +0800

    member_template

--- tmplate_base/member_template/member_template.hpp
+++ tmplate_base/member_template/member_template.hpp
@@ -6,10 +6,16 @@ class Stack {
 private:
     std::deque<T> elems;
 public:
-    void push(T const&);
-    void pop();
-    T top() const;
-
+    void push(T const& t) {
+        elems.push_back(t);
+    }
+    void pop() {
+        elems.pop_front();
+    }
+    T top() const {
+        return elems.front();
+    }
+    bool empty() { return elems.empty(); }
     template <typename T2>
     Stack<T> & operator= (Stack<T2> const&);
 };
@@ -31,18 +37,89 @@ Stack<T>& Stack<T>::operator= (Stack<T2> const& op2) {
     return *this;
 }
 
+//---StackPro-------------------------------------------------
 template<typename T, typename CONT = std::deque<T> >
 class StackPro{
 private:
     CONT elems;
 public:
+    void push(T t) {
+        elems.push_back(t);
+    }
+    void pop() {
+        elems.pop_front();
+    }
+    T top() const {
+        return elems.front();
+    }
+    bool empty() {
+        return elems.empty();
+    }
+
     template<typename T2, typename CONT2>
-    StackPro<T, CONT>& operator= (StackPro<T2, CONT2>conts&);
+    StackPro<T, CONT>const & operator= (StackPro<T2, CONT2> const & op2);
 };
 
 template<typename T, typename CONT>
 template<typename T2, typename CONT2>
-StackPro<T, CONT>& 
-StackPro<T, CONT>::operator= (StackPro<T2, CONT2>conts& op2) {
+inline StackPro<T, CONT>const & StackPro<T, CONT>::operator=(StackPro<T2, CONT2> const & op2)
+{
+    elems.clear();
+    StackPro<T2, CONT2>tmp(op2);
+    while (!tmp.empty()) {
+        push(tmp.top());
+        tmp.pop();
+    }
+
     return *this;
 }
+
+//---StackProS-------------------------------------------------
+template<typename T,
+    template<typename ELEM,
+    typename ALLOC = std::allocator<ELEM> > class CONT = std::deque >
+    class StackProS {
+    private:
+        CONT<T> elems;
+    public:
+        
+        void pop() {
+            elems.pop_front();
+        }
+        T top() const {
+            return elems.front();
+        }
+        bool empty() {
+            return elems.empty();
+        }
+
+        void push(T t);
+
+        template<typename T2,
+            template<typename ELEM2,
+            typename ALLOC2 = std::allocator<ELEM2> > class CONT2 >
+            StackProS<T, CONT>& operator= (StackProS<T2, CONT2> const&);
+};
+
+template<typename T, template<typename, typename> class CONT>
+void StackProS<T, CONT>::push(T t) {
+    elems.push_back(t);
+}
+
+template<typename T, template<typename, typename> class CONT >
+template<typename T2, template<typename, typename>class CONT2 >
+StackProS<T, CONT>&
+StackProS<T, CONT>::operator= (StackProS<T2, CONT2> const& op2) {
+    if ((void*)this == (void*)&op2) {
+        return *this;
+    }
+
+    StackProS<T2, CONT2> tmp(op2);
+    elems.clear();
+    while (!tmp.empty()) {
+        push(tmp.top());
+        tmp.pop();
+    }
+    return *this;
+}
+
